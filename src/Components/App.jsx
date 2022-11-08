caf22bcdaa038511df69cc9fde2094fa3e89b4de
Tim Traylor <ttraylor83@gmail.com>
2022-11-08 01:56:16 +0000

    base code layout for redux

---
+++ src/Components/App.jsx
@@ -0,0 +1,18 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Products from './Products/Products';
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      Testing
+      <Products />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

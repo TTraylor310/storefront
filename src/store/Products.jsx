98b339eb8e76ca5d978e67afe67ffc1bc94d0deb
Tim Traylor <ttraylor83@gmail.com>
2022-11-09 04:04:43 +0000

    decent organized and working functionality

--- src/store/Products.jsx
+++ src/store/Products.jsx
@@ -1,10 +1,4 @@
-let initialState = {
-  categories: [
-    { name: 'electronics', displayName: 'Electronics' },
-    { name: 'food', displayName: 'Food' },
-    { name: 'clothing', displayName: 'Clothing' },
-  ],
-  products: [
+const initialState = [
     { name: 'TV', category: 'electronics', price: 699.00, inStock: 5 },
     { name: 'Radio', category: 'electronics', price: 99.00, inStock: 15 },
     { name: 'Shirt', category: 'clothing', price: 9.00, inStock: 25 },
@@ -12,44 +6,12 @@ let initialState = {
     { name: 'Apples', category: 'food', price: .99, inStock: 500 },
     { name: 'Eggs', category: 'food', price: 1.99, inStock: 12 },
     { name: 'Bread', category: 'food', price: 2.39, inStock: 90 },
-  ],
-  activeCategory: '',
-};
-
+];
 
 function productsReducer(state = initialState, action) {
-  const { type, payload } = action;
-
-  switch (type) {
-    case 'electronics':
-      return {
-        ...state,
-        products: state.products.filter(product => 
-          product.category === 'electronics'
-        ),
-        activeCategory: 'electronics',
-      }
-
-    case 'clothing':
-      return {
-        ...state,
-        products: state.products.filter(product => 
-          product.category === 'clothing'
-        ),
-        activeCategory: 'clothing',
-      }
-
-      case 'food':
-        return {
-          ...state,
-          products: state.products.filter(product => 
-            product.category === 'food'
-          ),
-          activeCategory: 'food',
-        }
-
-    case 'RESET':
-      return initialState;
+    switch (action.type) {
+    case 'SELECT_CATEGORY':
+      return initialState.filter(product => product.category === action.payload)
 
     default:
       return state;
@@ -58,27 +20,10 @@ function productsReducer(state = initialState, action) {
 
 export default productsReducer;
 
-export const chooseElec = (products) => {
-  return {
-    type: 'electronics',
-    payload: products,
-  }
-}
-export const chooseCloth = (products) => {
-  return {
-    type: 'clothing',
-    payload: products,
-  }
-}
-export const chooseFood = (products) => {
-  return {
-    type: 'food',
-    payload: products,
-  }
-}
 
 export const reset = () => {
   return {
     type: 'RESET',
+    payload: initialState
   }
 }
--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,11 +1,21 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import productsReducer from './Products'
+import productsReducer from './Products';
+import categoryReducer from './Categories';
+
 
 let reducers = combineReducers({
   products: productsReducer,
+  category: categoryReducer
 });
 
 export default function store() {
   return createStore(reducers, composeWithDevTools());
+}
+
+export const selectCategory = (category) => {
+  return {
+    type: 'SELECT_CATEGORY',
+    payload: category
+  }
 }
